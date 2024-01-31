bea4ecbb7f76eec051b082e28ab2305d38d6e046
Outro-Dev <outroluiz@gmail.com>
2024-01-31 01:26:09 -0300

    "test(autocomplete): implement filter functionality test"

--- src/pages/teste.jsx
+++ src/pages/teste.jsx
@@ -1,53 +1,88 @@
-import React, { useState } from 'react'
-import { Button, Dialog, Typography, Box, Paper } from '@mui/material'
+// ------ TESTANDO IMPLEMENTAÇÃO DE AMANHÃ - VALORES NAS TAGS ----
 
-import {
-  ConfimationDialog,
-  DeleteDialog,
-  DetailsDialog,
-  FormDialog,
-} from '@/components'
+import Autocomplete from '@mui/material/Autocomplete'
+import TextField from '@mui/material/TextField'
+import Typography from '@mui/material/Typography'
+import { useState } from 'react'
 
-const teste = () => {
-  const [isOpen, setIsOpen] = useState(false)
+const availableTags = ['WEB', 'UI', 'UX']
 
-  const handleOpen = () => {
-    setIsOpen(true)
+function TagsValues() {
+  const [selectedValues, setSelectedValues] = useState([])
+
+  const handleChange = (event, newValue) => {
+    // Limitar a seleção a no máximo três TAGs
+    if (newValue.length <= 3) {
+      setSelectedValues(newValue)
+    }
   }
 
-  const handleClose = () => {
-    setIsOpen(false)
-  }
+  const remainingTags = availableTags.filter(
+    (tag) => !selectedValues.includes(tag),
+  )
+
   return (
-    <>
-      <Button variant="contained" color="primary" onClick={handleOpen}>
-        Teste
-      </Button>
+    <div>
+      <Autocomplete
+        style={{ width: '300px' }}
+        multiple
+        id="tags-outlined"
+        options={remainingTags}
+        getOptionLabel={(option) => option}
+        onChange={handleChange}
+        value={selectedValues}
+        noOptionsText="Você atingiu o limite de TAGs" // Faz aparecer o texto depois de atingir o limite de TAGs
+        renderInput={(params) => (
+          <TextField {...params} label="Tags" placeholder="Tags" />
+        )}
+      />
 
-      {/*  <ConfimationDialog
-        open={isOpen}
-        onClose={handleClose}
-        onClick={handleClose}
-      /> */}
-
-      {/*  <DeleteDialog
-        open={isOpen}
-        onClick={handleClose}
-        onClose={handleClose}
-      /> */}
-
-      {/*  <FormDialog
-        open={isOpen}
-        onClose={handleClose}
-        onClick={handleClose}
-      /> */}
-
-      {/* <DetailsDialog
-        open={isOpen}
-        onClose={handleClose}
-      /> */}
-    </>
+      {selectedValues.length > 0 && (
+        // Se o comprimento de SelectedValues é maior que Zero, chama Typography
+        <Typography variant="inherit" style={{ marginTop: '16px' }}>
+          Valores Selecionados:
+        </Typography>
+      )}
+      <ul>
+        {selectedValues.map((value, index) => (
+          <li key={index}>{value}</li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
-export default teste
+export default TagsValues
+
+// const teste = () => {
+//   const [isOpen, setIsOpen] = useState(false)
+
+//   const handleOpen = () => {
+//     setIsOpen(true)
+//   }
+
+//   const handleClose = () => {
+//     setIsOpen(false)
+//   }
+//   return (
+//     <>
+//       <Button variant="contained" color="primary" onClick={handleOpen}>
+//         Teste
+//       </Button>
+
+//       <ConfimationDialog
+//         open={isOpen}
+//         onClose={handleClose}
+//         onClick={handleClose}
+//       />
+
+//       <DeleteDialog open={isOpen} onClick={handleClose} onClose={handleClose} />
+
+//       <FormDialog open={isOpen} onClose={handleClose} onClick={handleClose} />
+
+//       <DetailsDialog open={isOpen} onClose={handleClose} />
+//     </>
+//   )
+// }
+
+// export default teste
